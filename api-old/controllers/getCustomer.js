cd1c023bd3d19d15a7b5dcd588306a655fd7fabe
Christian Schlosrich <christian.schlosrich@gmail.com>
2016-06-11 17:02:24 +1000

    Clean up

--- api-old/controllers/getCustomer.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    get: function(req, res) {
-        res.json({
-            firstName: 'Jackie',
-            lastName: 'Smith'
-        });
-    }
-};
--- gulptasks/buildYaml.js
+++ gulptasks/buildYaml.js
@@ -1,10 +1,10 @@
-var gulp = require('gulp');
-var resolve = require('json-refs').resolveRefs;
-var YAML = require('yaml-js');
-var fs = require('fs');
-var toYaml = require('yamljs');
+var gulp = require('gulp')
+    , resolve = require('json-refs').resolveRefs
+    , YAML = require('yaml-js')
+    , fs = require('fs')
+    , toYaml = require('yamljs')
+    , root = YAML.load(fs.readFileSync('./api/index.yaml').toString());
 
-var root = YAML.load(fs.readFileSync('./api/index.yaml').toString());
 var options = {
     filter        : ['relative', 'remote'],
     loaderOptions : {
@@ -16,22 +16,12 @@ var options = {
 
 resolve(root, options).then(function (results) {
 
-    // console.log(results.resolved)
+    var yaml = toYaml.stringify(results.resolved, 8, 2);
 
-    // var burger = toYaml.stringify(results.resolved, [1, 1]);
-    var burger = toYaml.stringify(results.resolved, 8, 2);
-
-    console.log(burger)
-    // yamlString = YAML.stringify(nativeObject[, inline /* @integer depth to start using inline notation at */[, spaces /* @integer number of spaces to use for indentation */] ]);
-
-    var data = JSON.stringify(results.resolved, null, 2);
-
-    fs.writeFile('./api/swagger/swagger.yaml', burger, function(err){
+    fs.writeFile('./api/swagger/swagger.yaml', yaml, function(err){
 
         if(err){
             throw err;
         }
-
-        console.log('Build service file ok')
     });
 });
